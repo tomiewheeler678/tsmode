8a18d6a3bba7e983b851b4fa1b3119f01b661831
tomiewheeler678 <tomiewheeler678@gmail.com>
2019-08-11 20:07:46 +0000

    style the app

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,33 +1,21 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-
-  def destroy
-    @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
-    return render_not_found(:forbidden) if @gram.user != current_user
-    @gram.destroy
-    redirect_to root_path
+  
+  def index
+    @grams = Gram.all
   end
-
-
-  def update
-    @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
-    return render_not_found(:forbidden) if @gram.user != current_user
-
-    @gram.update_attributes(gram_params)
-    if @gram.valid?
-      redirect_to root_path
-    else
-      return render :edit, status: :unprocessable_entity
-    end
-  end
-
+  
   def new
     @gram = Gram.new
   end
 
-  def index
+  def create
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -41,23 +29,30 @@ class GramsController < ApplicationController
     return render_not_found(:forbidden) if @gram.user != current_user
   end
 
-def create
-  @gram = current_user.grams.create(gram_params)
-  if @gram.valid?
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
   end
-end
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
+    redirect_to root_path
+  end
 
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
-
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
-  
 end
